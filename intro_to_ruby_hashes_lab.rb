a269487c43fac39158d7978b086cd1239d5e20e2
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 00:26:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,16 +36,16 @@ def monopoly_with_fourth_tier
 	    },
 	    names: {
 	      reading_railroad: {
-	        mortgage_value: "$100"
+	        "mortgage_value": "$100"
 	      },
 	      pennsylvania_railroad: {
-	        mortgage_value: "$100"
+	        "mortgage_value": "$100"
 	      },
 	      b_and_o_railroad: {
-	        mortgage_value: "$"
+	        "mortgage_value": "$"
 	      },
 	      shortline_railroad: {
-	        mortgage_value: "$"
+	        "mortgage_value": "$"
 	      }
 	    }
 	  }
